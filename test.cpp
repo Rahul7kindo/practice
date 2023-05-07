4649b890b5cd3aa1dcc7acb5551ccf41e9805e38
Rahul7kindo <122984724+Rahul7kindo@users.noreply.github.com>
2023-05-07 09:33:26 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<math.h>
+using namespace std;
+
+
+
+int main()
+{   
+    int row,col;
+    cin>>row;
+    cin>>col;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    
+
+    
+    
+
+    return 0;
+}
+
