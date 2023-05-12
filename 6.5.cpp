68ec6db6f1028a7d2746aea1b6f42b5bca110f00
Rahul7kindo <122984724+Rahul7kindo@users.noreply.github.com>
2023-05-12 11:43:32 +0530

    Add files via upload

---
+++ 6.5.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<math.h>
+using namespace std;
+
+
+
+int main()
+{   
+    int n;
+    cin>>n;
+    int cnt=1;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<cnt<<" ";
+            cnt++;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+  
+
+    
+    
+
+    return 0;
+}
