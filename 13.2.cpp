27bafeb882a7b4559540386f4163d637a2d95949
Rahul7kindo <122984724+Rahul7kindo@users.noreply.github.com>
2023-05-26 14:03:26 +0530

    Add files via upload

---
+++ 13.2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+#include<stack>
+#include<queue>
+#include<math.h>
+using namespace std;
+
+
+int main()
+{   
+    int n;
+    cin>>n;
+    int res=0;
+
+    while(n!=0)
+    {
+        res=((res*10)+(n%10));
+        n/=10;
+    }
+    cout<<res<<endl;
+
+
+    return 0;
+}
+
